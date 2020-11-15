3ab2eeaa875aa8482f211d42f87b743fc0a21b2c
Robert Chaim <robak3731@gmail.com>
2020-11-15 13:34:53 +0100

    Added menu

--- Calculator.py
+++ Calculator.py
@@ -20,45 +20,55 @@ def divide(num1, num2: float) -> float:
     return num1 / num2
 
 
-select = int(input('''
-Select operations form: 
-1. Add 
-2. Subtract
-3. Multiply
-4. Divide
-'''))
+def menu():
+    while True:
+        select = int(input('''
+        Select operations form: 
+        1. Add 
+        2. Subtract
+        3. Multiply
+        4. Divide
+        5. Exit
+        '''))
+
+        if select == 1:
+            try:
+                numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
+            except ValueError:
+                print("Entered values cannot be added. Please enter correct values.")
+                numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
+            finally:
+                logging.info("Adding {}".format(numbers_list))
+                print('Result {}'.format(add(numbers_list)))
+
+        elif select == 2:
+            number_1 = float(input("Enter first number: "))
+            number_2 = float(input("Enter second number: "))
+            logging.info("Subtracting {} and {}".format(number_1, number_2))
+            print('Result {}'.format(subtract(number_1, number_2)))
+
+        elif select == 3:
+            try:
+                numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
+            except ValueError:
+                print("Entered values cannot be multiplied. Please enter correct values.")
+                numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
+            finally:
+                logging.info("Multiplying {}".format(numbers_list))
+                print('Result {}'.format(multiply(numbers_list)))
+
+        elif select == 4:
+            number_1 = float(input("Enter first number: "))
+            number_2 = float(input("Enter second number: "))
+            logging.info("Dividing {} and {}".format(number_1, number_2))
+            print('Result {}'.format(divide(number_1, number_2)))
+
+        elif select == 5:
+            return False
+
+        else:
+            print("Invalid input")
 
 
-if select == 1:
-    try:
-        numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
-    except ValueError:
-        print("Entered values cannot be added. Please enter correct values.")
-        numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
-    finally:
-        logging.info("Adding {}".format(numbers_list))
-        print('Result {}'.format(add(numbers_list)))
-
-elif select == 2:
-    number_1 = float(input("Enter first number: "))
-    number_2 = float(input("Enter second number: "))
-    logging.info("Subtracting {} and {}".format(number_1, number_2))
-    print('Result {}'.format(subtract(number_1, number_2)))
-
-elif select == 3:
-    try:
-        numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
-    except ValueError:
-        print("Entered values cannot be multiplied. Please enter correct values.")
-        numbers_list = list(map(float, input("Enter a multiple value(ex. 1 2.3 13): ").split()))
-    finally:
-        logging.info("Multiplying {}".format(numbers_list))
-        print('Result {}'.format(multiply(numbers_list)))
-
-elif select == 4:
-    number_1 = float(input("Enter first number: "))
-    number_2 = float(input("Enter second number: "))
-    logging.info("Dividing {} and {}".format(number_1, number_2))
-    print('Result {}'.format(divide(number_1, number_2)))
-else:
-    print("Invalid input")
+if __name__ == '__main__':
+    menu()
